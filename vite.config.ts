4736903e61d990f9b28445d22d011d0799788b34
zhangrixin <zhangrixin@bilibili.com>
2025-04-10 10:07:06 +0800

    /

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,25 @@
 import { defineConfig } from 'vite'
+import path from 'path' //这个path用到了上面安装的@types/node
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    })
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve('./src'), // @代替src
+      '#': path.resolve('./types') // #代替types
     }
-  },
-  server: {
-    port: 5173,
-    open: true // 自动打开浏览器
   }
-}) 
+})
